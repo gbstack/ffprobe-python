d39987e031be51422cc9df1f7f03e0e2926a28b4
Simon Hargreaves <simon@simon-hargreaves.com>
2013-06-01 20:06:46 +0100

    added setup for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='ffprobe',
+    version='0.1',
+    description='Wrapper around ffprobe command to extract metadata from media files',
+    author='Simon Hargreaves',
+    author_email='simon@simon-hargreaves.com',
+    url='http://www.simon-hargreaves.com/ffprobe',
+    packages=['ffprobe'])
