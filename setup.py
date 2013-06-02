b03db1d0d04310248dee8a00d330375403665027
Simon Hargreaves <simon@simon-hargreaves.com>
2013-06-02 06:49:20 +0100

    Packaging update
    Package updates and pushed to pypi

--- setup.py
+++ setup.py
@@ -9,4 +9,17 @@ setup(
     author='Simon Hargreaves',
     author_email='simon@simon-hargreaves.com',
     url='http://www.simon-hargreaves.com/ffprobe',
-    packages=['ffprobe'])
+    packages=['ffprobe'],
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Environment :: Console',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+		'Operating System :: POSIX',
+		'Programming Language :: Python',
+		'Natural Language :: English',
+		'Topic :: Multimedia :: Video',
+		'Topic :: Software Development :: Libraries'
+		])
